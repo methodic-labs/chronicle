f97a0fe3df782d91b45dbf30ecc0d9924e4163bd
Matthew Tamayo-Rios <matthew@openlattice.com>
2018-05-03 23:05:47 -0700

    Only collect usage stats if screen is on

--- app/src/main/java/com/openlattice/chronicle/services/usage/UsageService.kt
+++ app/src/main/java/com/openlattice/chronicle/services/usage/UsageService.kt
@@ -3,6 +3,7 @@ package com.openlattice.chronicle.services.usage
 import android.app.job.JobParameters
 import android.app.job.JobService
 import android.arch.persistence.room.Room
+import android.content.Context
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
@@ -22,6 +23,10 @@ import java.util.*
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
+import android.view.Display
+import android.content.Context.DISPLAY_SERVICE
+import android.hardware.display.DisplayManager
+
 
 const val LIFETIME = 60 * 1000
 
@@ -42,6 +47,10 @@ class UsageService : JobService() {
     lateinit var sensors: Set<ChronicleSensor>
 
     override fun onStartJob(params: JobParameters?): Boolean {
+        if (isScreenOff()) {
+            return true
+        }
+
         executor.execute {
             propertyTypeIds = createRetrofitAdapter(PRODUCTION).create(ChronicleApi::class.java).getPropertyTypeIds(FQNS)
             chronicleDb = Room.databaseBuilder(applicationContext, ChronicleDb::class.java!!, "chronicle").build()
@@ -57,6 +66,15 @@ class UsageService : JobService() {
         return true
     }
 
+    fun isScreenOff(): Boolean {
+        val dm = applicationContext.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+        for (display in dm.displays) {
+            if (display.state == Display.STATE_ON) {
+                return false
+            }
+        }
+        return true
+    }
 
     override fun onStopJob(params: JobParameters?): Boolean {
         Log.i(javaClass.name, "Stop requested after ${sw.elapsed(TimeUnit.SECONDS)}")
@@ -88,7 +106,7 @@ class UsageService : JobService() {
 
             Log.i(javaClass.name, "Collecting Usage Information. This service has been running for ${sw.elapsed(TimeUnit.SECONDS)} seconds.")
 
-            handler.postDelayed(this::doWork,  1000)
+            handler.postDelayed(this::doWork, 1000)
         } else {
             shutdownLatch.countDown()
         }
