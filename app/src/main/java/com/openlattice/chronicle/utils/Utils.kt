6a0a31c5bd8f6714af4ace4f252350413c82f22c
Alfonce Nzioka <alfonce.starfish@gmail.com>
2020-01-29 23:24:44 -0800

    Add application name to upload payload

--- app/src/main/java/com/openlattice/chronicle/utils/Utils.kt
+++ app/src/main/java/com/openlattice/chronicle/utils/Utils.kt
@@ -1,5 +1,7 @@
 package com.openlattice.chronicle.utils
 
+import android.content.Context
+import android.content.pm.PackageManager
 import com.crashlytics.android.Crashlytics
 
 import java.util.UUID
@@ -14,5 +16,11 @@ object Utils {
     catch (e: IllegalArgumentException) {
         Crashlytics.logException(e)
         false
+    }
+
+    fun getAppFullName(context :Context, packageName: String) :String  {
+        val packageManager = context.applicationContext.packageManager
+        val applicationInfo = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
+        return  packageManager.getApplicationLabel(applicationInfo).toString()
     }
 }
