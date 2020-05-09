d3aa3534b083e96b86e5f60bc674c8fc65118258
Alfonce Nzioka <=>
2020-05-08 22:22:13 -0700

    stop collecting and uploading usage data when participant is not enrolled

--- app/src/main/java/com/openlattice/chronicle/receivers/lifecycle/StartOnBoot.kt
+++ app/src/main/java/com/openlattice/chronicle/receivers/lifecycle/StartOnBoot.kt
@@ -6,6 +6,9 @@ import android.content.Intent
 import android.content.Intent.ACTION_BOOT_COMPLETED
 import android.util.Log
 import com.openlattice.chronicle.services.notifications.scheduleNotificationJobService
+import com.openlattice.chronicle.data.ParticipationStatus
+import com.openlattice.chronicle.preferences.EnrollmentSettings
+import com.openlattice.chronicle.services.status.scheduleParticipationStatusJob
 import com.openlattice.chronicle.services.upload.scheduleUploadJob
 import com.openlattice.chronicle.services.usage.scheduleUsageMonitoringJob
 
@@ -13,16 +16,22 @@ class StartOnBoot : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         if (context != null && intent != null) {
             if (intent.action.equals(ACTION_BOOT_COMPLETED)) {
-                scheduleUsageMonitoringJob(context)
-                Log.i(javaClass.canonicalName, "Started usage service at boot.")
-                scheduleUploadJob(context)
-                Log.i(javaClass.canonicalName, "Scheduled upload job at boot.")
+                val settings = EnrollmentSettings(context)
+
+                if (settings.getParticipationStatus() == ParticipationStatus.ENROLLED) {
+                    scheduleUsageMonitoringJob(context)
+                    Log.i(javaClass.canonicalName, "Started usage service at boot.")
+                    scheduleUploadJob(context)
+                    Log.i(javaClass.canonicalName, "Scheduled upload job at boot.")
+                }
                 scheduleNotificationJobService(context)
                 Log.i(javaClass.name, "Scheduled notification service at boot")
+
+                scheduleParticipationStatusJob(context)
+                Log.i(javaClass.name, "Scheduled participation status service at boot")
             }
         } else {
             Log.e(javaClass.canonicalName, "Unable to start Usage Service at Boot.")
         }
     }
 }
-
