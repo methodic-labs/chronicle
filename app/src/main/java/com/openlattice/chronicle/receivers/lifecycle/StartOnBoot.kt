02adf3e2000e0f8f358178aaa02d8614506d4e01
Alfonce Nzioka <=>
2020-05-12 14:39:16 -0700

    clean up

--- app/src/main/java/com/openlattice/chronicle/receivers/lifecycle/StartOnBoot.kt
+++ app/src/main/java/com/openlattice/chronicle/receivers/lifecycle/StartOnBoot.kt
@@ -5,9 +5,9 @@ import android.content.Context
 import android.content.Intent
 import android.content.Intent.ACTION_BOOT_COMPLETED
 import android.util.Log
-import com.openlattice.chronicle.services.notifications.scheduleNotificationJobService
 import com.openlattice.chronicle.data.ParticipationStatus
 import com.openlattice.chronicle.preferences.EnrollmentSettings
+import com.openlattice.chronicle.services.notifications.scheduleNotificationJobService
 import com.openlattice.chronicle.services.status.scheduleParticipationStatusJob
 import com.openlattice.chronicle.services.upload.scheduleUploadJob
 import com.openlattice.chronicle.services.usage.scheduleUsageMonitoringJob
@@ -17,16 +17,16 @@ class StartOnBoot : BroadcastReceiver() {
         if (context != null && intent != null) {
             if (intent.action.equals(ACTION_BOOT_COMPLETED)) {
                 val settings = EnrollmentSettings(context)
-
                 if (settings.getParticipationStatus() == ParticipationStatus.ENROLLED) {
                     scheduleUsageMonitoringJob(context)
                     Log.i(javaClass.canonicalName, "Started usage service at boot.")
+
                     scheduleUploadJob(context)
                     Log.i(javaClass.canonicalName, "Scheduled upload job at boot.")
                 }
                 scheduleNotificationJobService(context)
                 Log.i(javaClass.name, "Scheduled notification service at boot")
-
+                
                 scheduleParticipationStatusJob(context)
                 Log.i(javaClass.name, "Scheduled participation status service at boot")
             }
