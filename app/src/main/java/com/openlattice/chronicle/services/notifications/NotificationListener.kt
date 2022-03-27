69dab8c95134788a59f540ed7d0f12d7ddf6d749
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-03-27 14:42:00 -0700

    comment

--- app/src/main/java/com/openlattice/chronicle/services/notifications/NotificationListener.kt
+++ app/src/main/java/com/openlattice/chronicle/services/notifications/NotificationListener.kt
@@ -8,6 +8,9 @@ import com.openlattice.chronicle.preferences.EnrollmentSettings
 
 class NotificationListener : NotificationListenerService() {
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
+
+        // When notification to identify user has been posted, unassign current user. If user ignores notification,
+        // subsequent usage events will be assumed to belong to an 'unidentified user'
         if (sbn?.id == applicationContext.resources.getInteger(R.integer.identify_user_notification_id)) {
             Log.i(javaClass.name, "User identification notification posted.")
             EnrollmentSettings(applicationContext).setTargetUser(applicationContext.getString(R.string.user_unassigned))
