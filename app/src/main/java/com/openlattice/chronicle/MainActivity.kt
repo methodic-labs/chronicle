71d442f050f5b2629ca0dbd6aaea8a87a3bd87fa
Matthew Tamayo-Rios <matthew@openlattice.com>
2022-03-27 17:31:55 -0700

    Fix compile + add back in fqn

--- app/src/main/java/com/openlattice/chronicle/MainActivity.kt
+++ app/src/main/java/com/openlattice/chronicle/MainActivity.kt
@@ -174,7 +174,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.main_menu, menu)
         return true
