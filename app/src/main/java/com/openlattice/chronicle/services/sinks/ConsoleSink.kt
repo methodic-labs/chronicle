92b76733b7831c5a1dc1426a5dbd92a9d1b3a945
Matthew Tamayo-Rios <matthew@openlattice.com>
2022-03-31 00:53:16 -0700

    Fix console sink.

--- app/src/main/java/com/openlattice/chronicle/services/sinks/ConsoleSink.kt
+++ app/src/main/java/com/openlattice/chronicle/services/sinks/ConsoleSink.kt
@@ -1,25 +1,14 @@
 package com.openlattice.chronicle.services.sinks
 
 import android.util.Log
-import com.google.common.collect.SetMultimap
-<<<<<<< HEAD
 import com.openlattice.chronicle.android.ChronicleDataUpload
-=======
-import com.openlattice.chronicle.android.ChronicleUsageEvent
->>>>>>> e7c0ed09476a63cbd6b50ec78d74b7462710a223
 import com.openlattice.chronicle.util.RetrofitBuilders
-import retrofit2.Retrofit
-import java.util.*
 
 /*
  * This class is mainly for testing.
  */
 class ConsoleSink : DataSink {
-<<<<<<< HEAD
     override fun submit(data: List<ChronicleDataUpload>): Map<String, Boolean> {
-=======
-    override fun submit(data: List<ChronicleUsageEvent>): Map<String, Boolean> {
->>>>>>> e7c0ed09476a63cbd6b50ec78d74b7462710a223
         Log.d(javaClass.name, RetrofitBuilders.mapper.writeValueAsString(data))
         return mapOf(ConsoleSink::class.java.name to true )
     }
