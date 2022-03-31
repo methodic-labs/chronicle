3926de156c8ebdc220b09b2f87013abc4510bf84
Matthew Tamayo-Rios <matthew@openlattice.com>
2022-03-31 00:53:39 -0700

    Fix data sink.

--- app/src/main/java/com/openlattice/chronicle/services/sinks/DataSink.kt
+++ app/src/main/java/com/openlattice/chronicle/services/sinks/DataSink.kt
@@ -1,10 +1,6 @@
 package com.openlattice.chronicle.services.sinks
 
-<<<<<<< HEAD
 import com.openlattice.chronicle.android.ChronicleDataUpload
-=======
-import com.openlattice.chronicle.android.ChronicleUsageEvent
->>>>>>> e7c0ed09476a63cbd6b50ec78d74b7462710a223
 
 interface DataSink {
     fun submit(data: List<ChronicleDataUpload>): Map<String, Boolean>
