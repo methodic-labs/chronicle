4a84332110d7a43750c33542c6da1ace159f6c2b
Matthew Tamayo-Rios <matthew@openlattice.com>
2022-03-31 00:48:55 -0700

    Fix uncorrected merge conflict issues

--- app/src/main/java/com/openlattice/chronicle/services/sinks/BrokerDataSink.kt
+++ app/src/main/java/com/openlattice/chronicle/services/sinks/BrokerDataSink.kt
@@ -1,11 +1,8 @@
 package com.openlattice.chronicle.services.sinks
 
 import com.google.common.collect.SetMultimap
-<<<<<<< HEAD
 import com.openlattice.chronicle.android.ChronicleDataUpload
-=======
 import com.openlattice.chronicle.android.ChronicleUsageEvent
->>>>>>> e7c0ed09476a63cbd6b50ec78d74b7462710a223
 import java.util.*
 
 
@@ -16,11 +13,9 @@ class BrokerDataSink(private val dataSinks: MutableSet<DataSink>) : DataSink {
         dataSinks.add(dataSink)
     }
 
-<<<<<<< HEAD
     override fun submit(data: List<ChronicleDataUpload>): Map<String, Boolean> {
-=======
-    override fun submit(data: List<ChronicleUsageEvent>): Map<String, Boolean> {
->>>>>>> e7c0ed09476a63cbd6b50ec78d74b7462710a223
-        return dataSinks.asSequence().flatMap { it.submit(data).asSequence().map { it.key to it.value } }.toMap()
+        return dataSinks.asSequence()
+            .flatMap { it.submit(data).asSequence() }
+            .associate { it.key to it.value }
     }
 }
