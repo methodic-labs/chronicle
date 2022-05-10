814b542877d258f3575385b07e985143c1876a78
Alfonce Nzioka <alfonce.starfish@gmail.com>
2022-05-09 20:51:31 -0700

    migrate database

--- app/src/main/java/com/openlattice/chronicle/storage/ChronicleDb.kt
+++ app/src/main/java/com/openlattice/chronicle/storage/ChronicleDb.kt
@@ -1,9 +1,16 @@
 package com.openlattice.chronicle.storage
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [QueueEntry::class, UserQueueEntry::class], version = 1)
+@Database(
+    entities = [QueueEntry::class, UserQueueEntry::class],
+    version = 2,
+    autoMigrations = [
+        AutoMigration (from = 1, to = 2)
+    ]
+)
 abstract class ChronicleDb : RoomDatabase() {
     abstract fun queueEntryData(): StorageQueue
     abstract fun userQueueEntryData(): UserStorageQueue
