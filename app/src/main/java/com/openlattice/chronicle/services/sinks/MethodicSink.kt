3f54cad29abbf0875500eb41c1913f42317d408c
Matthew Tamayo-Rios <matthew@geekbeast.com>
2022-10-06 12:02:28 -1000

    Log actual stack trace for failed uploads.

--- app/src/main/java/com/openlattice/chronicle/services/sinks/MethodicSink.kt
+++ app/src/main/java/com/openlattice/chronicle/services/sinks/MethodicSink.kt
@@ -1,21 +1,39 @@
 package com.openlattice.chronicle.services.sinks
 
+import android.os.Bundle
 import android.util.Log
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.openlattice.chronicle.android.ChronicleData
 import com.openlattice.chronicle.android.ChronicleSample
+import com.openlattice.chronicle.constants.FirebaseAnalyticsEvents
+import com.openlattice.chronicle.preferences.PARTICIPANT_ID
+import com.openlattice.chronicle.preferences.STUDY_ID
 import com.openlattice.chronicle.study.StudyApi
 import java.util.*
 
 class MethodicSink(
-    private var studyId: UUID,
-    private var participantId: String,
-    private var deviceId: String,
-    private var studyApi: StudyApi
+    private val studyId: UUID,
+    private val participantId: String,
+    private val deviceId: String,
+    private val studyApi: StudyApi,
+    private val crashlytics: FirebaseCrashlytics,
+    private val firebaseAnalytics: FirebaseAnalytics,
 ) : DataSink {
     override fun submit(data: List<ChronicleSample>): Map<String, Boolean> {
         val written = try {
-            studyApi.uploadAndroidUsageEventData( studyId, participantId, deviceId, ChronicleData( data) )
+            studyApi.uploadAndroidUsageEventData(
+                studyId,
+                participantId,
+                deviceId,
+                ChronicleData(data)
+            )
         } catch (e: Exception) {
+            crashlytics.recordException(e)
+            firebaseAnalytics.logEvent(FirebaseAnalyticsEvents.SUBMIT_FAILURE, Bundle().apply {
+                putString(PARTICIPANT_ID, participantId)
+                putString(STUDY_ID, studyId.toString())
+            })
             Log.i(javaClass.name, "Exception when uploading data", e)
             0
         }
