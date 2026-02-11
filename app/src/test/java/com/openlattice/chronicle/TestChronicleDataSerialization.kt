f9465fba93f2dfc0a435b6a52ab1ab99354db824
Matthew Tamayo-Rios <matthew@geekbeast.com>
2026-02-10 23:52:05 -0800

    Release commit.

--- app/src/test/java/com/openlattice/chronicle/TestChronicleDataSerialization.kt
+++ app/src/test/java/com/openlattice/chronicle/TestChronicleDataSerialization.kt
@@ -1,20 +1,22 @@
 package com.openlattice.chronicle
 
-import android.util.Log
-import android.util.Log.INFO
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.guava.GuavaModule
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.openlattice.chronicle.models.ExtractedUsageEvent
-import org.apache.commons.lang3.RandomStringUtils
-import org.junit.Test
-import java.time.OffsetDateTime
-import java.util.*
 import com.openlattice.chronicle.android.ChronicleData
+import com.openlattice.chronicle.models.ExtractedUsageEvent
+import com.openlattice.chronicle.serialization.JsonSerializer
+import com.openlattice.chronicle.sources.AndroidDevice
+import com.openlattice.chronicle.sources.SourceDevice
+import org.apache.commons.lang3.RandomStringUtils
 import org.junit.Assert
+import org.junit.Test
 import org.slf4j.LoggerFactory
+import java.time.OffsetDateTime
+import java.util.TimeZone
 
 
 class TestChronicleDataSerialization {
@@ -22,6 +24,7 @@ class TestChronicleDataSerialization {
         val mapper = ObjectMapper()
         val logger = LoggerFactory.getLogger(TestChronicleDataSerialization::class.java)
         init {
+            mapper.registerModule(KotlinModule.Builder().build())
             mapper.registerModule(GuavaModule())
             mapper.registerModule(JavaTimeModule())
             mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
@@ -48,5 +51,27 @@ class TestChronicleDataSerialization {
 
         val deserializedUsageEvents = mapper.readValue<ChronicleData>(json)
         Assert.assertEquals("Deserialized usage events must match." , usageEvents, deserializedUsageEvents )
+    }
+
+    @Test
+    fun testSourceDeviceSerialization() {
+        val device = AndroidDevice(
+            RandomStringUtils.randomAlphanumeric(5),
+            RandomStringUtils.randomAlphanumeric(5),
+            RandomStringUtils.randomAlphanumeric(5),
+            RandomStringUtils.randomAlphanumeric(5),
+            RandomStringUtils.randomAlphanumeric(5),
+            RandomStringUtils.randomAlphanumeric(5),
+            RandomStringUtils.randomAlphanumeric(5),
+            RandomStringUtils.randomAlphanumeric(5),
+            mapOf( RandomStringUtils.randomAlphanumeric(5) to RandomStringUtils.randomAlphanumeric(5))
+        )
+
+        val json = mapper.writeValueAsString( device )
+        println("Json: $json")
+        Assert.assertTrue( json.contains ("@class") )
+
+        val deserializedDevice = mapper.readValue<SourceDevice>(json)
+        Assert.assertEquals("Deserialized usage events must match." , device, deserializedDevice )
     }
 }
